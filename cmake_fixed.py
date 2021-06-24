4092e5319e78ae639e8a2a2d11df95fd7b31ccac
wt <wt1454246140@163.com>
2021-06-24 16:27:39 +0800

    Updated

--- cmake_fixed.py
+++ cmake_fixed.py
@@ -1,8 +1,30 @@
 #!/usr/bin/python
 
 #Fixed zephyr cmake generate error with no userspace
+#Sync zephyr west submodule config
 
 import os
+
+def delete_folder(path):
+  folder_name_exist = os.path.exists(path)
+  if not folder_name_exist:
+    return
+  for i in os.listdir(path):
+      path_file = os.path.join(path, i)
+      if os.path.isfile(path_file):
+          os.remove(path_file)
+      else:
+          delete_folder(path_file)
+  os.removedirs(path)
+
+def create_folder(path):
+    result_data = 0
+    folder_name_exist = os.path.exists(path)
+    if not folder_name_exist:
+        os.makedirs(path)
+    if not os.path.exists(path):
+        result_data = 1
+    return result_data
 
 def add_patch(path):
   with open(path, "r+", encoding="utf-8") as fd:
@@ -18,7 +40,19 @@ def fixed_cmake_file(root_path, *k):
     abs_path = os.path.join(root_path, "drivers/", dname, "CMakeLists.txt")
     add_patch(abs_path)
 
+def sync_west_config(path):
+  print ("update file: " + os.path.join(path, "config"))
+  with open(os.path.join(path, "config"), "w+", encoding="utf-8") as fd:
+    fd.seek(0, 0)
+    fd.write("[manifest]\npath = ../project_wkpace\nfile = west.yml\n\n[zephyr]\nbase = zephyr\n\n[build]\ngenerator = Unix Makefiles")
+
+def submodule_src_custom():
+  delete_folder(os.path.join(os.getcwd(), 'zephyr_pkg/.west'))
+  create_folder(os.path.join(os.getcwd(), 'zephyr_pkg/.west'))
+  sync_west_config(os.path.join(os.getcwd(), 'zephyr_pkg/.west'))
+
 if __name__ == "__main__":
   fixed_cmake_file(os.path.join(os.getcwd(), 'zephyr_pkg/zephyr'), 
     'gpio', 'flash', 'spi', 'serial', 'kscan', 'pwm')
-  
+  submodule_src_custom()
+
